5d1dfdf6d98e07b1b194eaca555941f4c72e07a9
vinsin2002 <vinsin2002@gmail.com>
2022-12-21 14:30:38 +0530

    fixes

--- Desert_Order/src/components/Meals/MealsSummary.js
+++ Desert_Order/src/components/Meals/MealsSummary.js
@@ -3,7 +3,7 @@ import classes from './MealsSummary.module.css';
 const MealsSummary = () => {
   return (
     <section className={classes.summary}>
-      <h2>Sweet you crave, Delivered To You</h2>
+      <h2>Sweets you crave, Delivered To You</h2>
       <p>
       Welcome to our online dessert shop! We offer a wide variety of delicious, handmade desserts that are sure to satisfy any sweet tooth. From creamy cheesecakes and rich chocolate brownies to fluffy cupcakes and moist layer cakes, we have something for everyone.
       </p>
